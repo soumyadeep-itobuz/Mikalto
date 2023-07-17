8b8498afc70b4f088681a187f6d79f502888cd53
soumyadeep-itobuz <soumyadeep@itobuz.com>
2023-07-17 11:21:12 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const navItems = document.querySelector(".nav-items");
+const room = document.querySelectorAll(".room");
+
+let roomIndex = 0;
+
+function hamburger() {
+  navItems.classList.toggle("show");
+}
+
+function showRoom(i) {
+  room[i].classList.remove("hidden");
+  for (let j = 0; j < i; j++) {
+    room[j].classList.add("hidden");
+  }
+  for (let j = i + 1; j < room.length; j++) {
+    room[j].classList.add("hidden");
+  }
+}
+function previousRoom() {
+  if (roomIndex === 0) {
+    roomIndex = room.length - 1;
+  } else {
+    roomIndex = roomIndex - 1;
+  }
+  showRoom(roomIndex);
+}
+
+function roomNext() {
+  if (roomIndex === room.length - 1) {
+    roomIndex = 0;
+  } else {
+    roomIndex = roomIndex + 1;
+  }
+  showRoom(roomIndex);
+}
+
+showRoom(roomIndex);
